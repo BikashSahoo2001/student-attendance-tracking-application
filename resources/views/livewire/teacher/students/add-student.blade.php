d46bf06e9ac4555c4c575296c37bac2014d2c561
Bikash718 <bikash.sahoo@magnigeeks.com>
2025-09-22 22:50:57 +0530

    grade file create and grade crud opeation done

--- resources/views/livewire/teacher/students/add-student.blade.php
+++ resources/views/livewire/teacher/students/add-student.blade.php
@@ -10,10 +10,15 @@
 
             <!-- Card -->
             <div class="mt-5 p-4 relative z-10 bg-white border border-gray-200 rounded-xl sm:mt-10 md:p-10 dark:bg-neutral-900 dark:border-neutral-700">
+
             <form wire:submit="save">
+                <a href="{{ route('student.index') }}" 
+                class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+                    View All
+                </a>
                 <div class="mb-4 sm:mb-8">
                 <label for="hs-feedback-post-comment-name-1" class="block mb-2 text-sm font-medium dark:text-white">First name</label>
-                <input type="text" wire:model="first_name" class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="First name">
+                <input type="text" wire:model="first_name" class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 bg-gray-50 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="First name">
                     @error('first_name')
                         <span class="text-red-500"> {{ $message }}</span>
                     @enderror
